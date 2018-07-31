eb02a9879b8f7ba112d0a9785c2872321262811e
Chris Constantine <cconstan@gmail.com>
2018-07-30 22:06:46 -0700

    updated pattern

--- scriptorium.ino
+++ scriptorium.ino
@@ -1,205 +1,106 @@
-#include <FastLED.h>
+/* noise16_1
+ * 
+ * By: Andrew Tuline
+ *
+ * Date: January, 2017
+ *
+ * A 16 bit noise routine with palettes. This was signficantly modified from a routine originally by Stefan Petrick.
+ * 
+ */
 
-#define LED_PIN     9
-#define BRIGHTNESS  96
-#define LED_TYPE    WS2811
-#define COLOR_ORDER GRB
+#include "FastLED.h"                                          // FastLED library.
 
-const uint8_t kMatrixWidth  = 138;
-const uint8_t kMatrixHeight = 1;
-const bool    kMatrixSerpentineLayout = true;
+#if FASTLED_VERSION < 3001000
+#error "Requires FastLED 3.1 or later; check github for latest code."
+#endif
 
+// Fixed definitions cannot change on the fly.
+#define LED_DT 9                                             // Data pin to connect to the strip.
+#define LED_CK 11                                             // Clock pin for WS2801 or APA102.
+#define COLOR_ORDER GRB                                       // It's GRB for WS2812 and BGR for APA102.
+#define LED_TYPE WS2812                                       // Using APA102, WS2812, WS2801. Don't forget to modify LEDS.addLeds to suit.
+#define NUM_LEDS 137                                           // Number of LED's.
 
-// This example combines two features of FastLED to produce a remarkable range of
-// effects from a relatively small amount of code.  This example combines FastLED's 
-// color palette lookup functions with FastLED's Perlin/simplex noise generator, and
-// the combination is extremely powerful.
-//
-// You might want to look at the "ColorPalette" and "Noise" examples separately
-// if this example code seems daunting.
-//
-// 
-// The basic setup here is that for each frame, we generate a new array of 
-// 'noise' data, and then map it onto the LED matrix through a color palette.
-//
-// Periodically, the color palette is changed, and new noise-generation parameters
-// are chosen at the same time.  In this example, specific noise-generation
-// values have been selected to match the given color palettes; some are faster, 
-// or slower, or larger, or smaller than others, but there's no reason these 
-// parameters can't be freely mixed-and-matched.
-//
-// In addition, this example includes some fast automatic 'data smoothing' at 
-// lower noise speeds to help produce smoother animations in those cases.
-//
-// The FastLED built-in color palettes (Forest, Clouds, Lava, Ocean, Party) are
-// used, as well as some 'hand-defined' ones, and some proceedurally generated
-// palettes.
+uint8_t max_bright = 255;
+
+CRGB leds[NUM_LEDS];
+
+CRGBPalette16 currentPalette;
+CRGBPalette16 targetPalette;
+CRGBPalette16 palette1 = LavaColors_p;
+CRGBPalette16 palette2 = HeatColors_p; 
+
+TBlendType    currentBlending;                                // NOBLEND or LINEARBLEND
+
+uint8_t maxChanges = 50;      // Number of tweens between palettes.
 
 
-#define NUM_LEDS (kMatrixWidth * kMatrixHeight)
-
-// The leds
-CRGB leds[kMatrixWidth * kMatrixHeight];
-
-// The 16 bit version of our coordinates
-static uint16_t x;
-static uint16_t y;
-static uint16_t z;
-
-// We're using the x/y dimensions to map to the x/y pixels on the matrix.  We'll
-// use the z-axis for "time".  speed determines how fast time moves forward.  Try
-// 1 for a very slow moving effect, or 60 for something that ends up looking like
-// water.
-uint16_t speed = 20; // speed is set dynamically once we've started up
-
-// Scale determines how far apart the pixels in our noise matrix are.  Try
-// changing these values around to see how it affects the motion of the display.  The
-// higher the value of scale, the more "zoomed out" the noise iwll be.  A value
-// of 1 will be so zoomed in, you'll mostly see solid colors.
-uint16_t scale = 30; // scale is set dynamically once we've started up
-
-// This is the array that we keep our computed noise values in
-uint8_t noise[kMatrixWidth][1];
-
-CRGBPalette16 currentPalette( LavaColors_p );
-uint8_t       colorLoop = 1;
 
 void setup() {
-  delay(3000);
-  LEDS.addLeds<LED_TYPE,LED_PIN,COLOR_ORDER>(leds,NUM_LEDS);
-  LEDS.setBrightness(BRIGHTNESS);
 
-  // Initialize our coordinates to some random values
-  x = random16();
-  y = random16();
-  z = random16();
-}
+  Serial.begin(57600);                                        // Initialize serial port for debugging.
+  delay(1000);                                                // Soft startup to ease the flow of electrons.
+ 
+ // LEDS.addLeds<LED_TYPE, LED_DT, LED_CK, COLOR_ORDER>(leds, NUM_LEDS);  // Use this for WS2801 or APA102
+ LEDS.addLeds<LED_TYPE, LED_DT, COLOR_ORDER>(leds, NUM_LEDS);  // Use this for WS2812
 
-
-
-// Fill the x/y array of 8-bit noise values using the inoise8 function.
-void fillnoise8() {
-  // If we're runing at a low "speed", some 8-bit artifacts become visible
-  // from frame-to-frame.  In order to reduce this, we can do some fast data-smoothing.
-  // The amount of data smoothing we're doing depends on "speed".
-  uint8_t dataSmoothing = 0;
-  if( speed < 50) {
-    dataSmoothing = 200 - (speed * 4);
-  }
+//  palette1 = 
+//  palette2 = palette1;//CRGBPalette16( CRGB::Green, CRGB::Blue, CRGB::Red);
+  currentPalette = palette1;
+  currentBlending = LINEARBLEND;  
   
-  for(int i = 0; i < kMatrixWidth; i++) {
-    int ioffset = scale * i;
-    for(int j = 0; j < kMatrixWidth; j++) {
-      int joffset = scale * j;
-      
-      uint8_t data = inoise8(x + ioffset,y + joffset,z);
+  FastLED.setBrightness(max_bright);
+  set_max_power_in_volts_and_milliamps(5, 1000);               // FastLED Power management set at 5V, 500mA.
 
-      // The range of the inoise8 function is roughly 16-238.
-      // These two operations expand those values out to roughly 0..255
-      // You can comment them out if you want the raw noise data.
-      data = qsub8(data,16);
-      data = qadd8(data,scale8(data,39));
+} // setup()
 
-      if( dataSmoothing ) {
-        uint8_t olddata = noise[i][j];
-        uint8_t newdata = scale8( olddata, dataSmoothing) + scale8( data, 256 - dataSmoothing);
-        data = newdata;
-      }
-      
-      noise[i][j] = data;
-    }
-  }
-  
-  z += speed;
-  
-  // apply slow drift to X and Y, just for visual variation.
-  x += speed / 8;
-  y -= speed / 16;
-}
 
-void mapNoiseToLEDsUsingPalette()
-{
-  static uint8_t ihue=0;
-  
-  for(int i = 0; i < kMatrixWidth; i++) {
-    for(int j = 0; j < kMatrixHeight; j++) {
-      // We use the value at the (i,j) coordinate in the noise
-      // array for our brightness, and the flipped value from (j,i)
-      // for our pixel's index into the color palette.
-
-      uint8_t index = noise[j][i];
-      uint8_t bri =   noise[i][j];
-
-      // if this palette is a 'loop', add a slowly-changing base value
-      if( colorLoop) { 
-        index += ihue;
-      }
-
-      // brighten up, as the color palette itself often contains the 
-      // light/dark dynamic range desired
-      if( bri > 127 ) {
-        bri = 255;
-      } else {
-        bri = dim8_raw( bri * 2);
-      }
-
-      CRGB color = ColorFromPalette( currentPalette, index, bri);
-      leds[XY(i,j)] = color;
-    }
-  }
-  
-  ihue+=1;
-}
 
 void loop() {
-  // Periodically choose a new palette, speed, and scale
-///  ChangePaletteAndSettingsPeriodically();
 
-  // generate noise data
-  fillnoise8();
-  
-  // convert the noise data to colors in the LED array
-  // using the current palette
-  mapNoiseToLEDsUsingPalette();
-
-  LEDS.show();
-  // delay(10);
-}
-
-
-
-// There are several different palettes of colors demonstrated here.
-//
-// FastLED provides several 'preset' palettes: RainbowColors_p, RainbowStripeColors_p,
-// OceanColors_p, CloudColors_p, LavaColors_p, ForestColors_p, and PartyColors_p.
-//
-// Additionally, you can manually define your own color palettes, or you can write
-// code that creates color palettes on the fly.
-
-// 1 = 5 sec per palette
-// 2 = 10 sec per palette
-// etc
-#define HOLD_PALETTES_X_TIMES_AS_LONG 1
-
-//
-// Mark's xy coordinate mapping code.  See the XYMatrix for more information on it.
-//
-uint16_t XY( uint8_t x, uint8_t y)
-{
-  uint16_t i;
-  if( kMatrixSerpentineLayout == false) {
-    i = (y * kMatrixWidth) + x;
+  EVERY_N_MILLISECONDS(50) {
+    nblendPaletteTowardPalette(currentPalette, targetPalette, maxChanges);  // Blend towards the target palette
   }
-  if( kMatrixSerpentineLayout == true) {
-    if( y & 0x01) {
-      // Odd rows run backwards
-      uint8_t reverseX = (kMatrixWidth - 1) - x;
-      i = (y * kMatrixWidth) + reverseX;
+
+ EVERY_N_SECONDS(5) {             // Change the target palette to a random one every 5 seconds.
+    if (targetPalette == palette1) {
+      targetPalette = palette2;
     } else {
-      // Even rows run forwards
-      i = (y * kMatrixWidth) + x;
+      targetPalette = palette1;
     }
   }
-  return i;
-}
+
+  noise16_1();
+  
+  FastLED.show();
+  
+} // loop()
+
+
+
+void noise16_1() {                                            // moves a noise up and down while slowly shifting to the side
+
+  uint16_t scale = 750;                                      // the "zoom factor" for the noise
+
+  for (uint16_t i = 0; i < NUM_LEDS; i++) {
+
+    uint16_t shift_x = beatsin8(3);     //5                      // the x position of the noise field swings @ 17 bpm
+    uint16_t shift_y = millis() / 500;                        // the y position becomes slowly incremented
+    
+
+    uint16_t real_x = (i + shift_x)*scale;                    // the x position of the noise field swings @ 17 bpm
+    uint16_t real_y = (i + shift_y)*scale;                    // the y position becomes slowly incremented
+    uint32_t real_z = millis() * 10;                          // the z position becomes quickly incremented
+    
+    uint8_t noise = inoise16(real_x, real_y, real_z) >> 8;   // get the noise data and scale it down
+
+    uint8_t index = sin8(noise*3);                           // map LED color based on noise data
+    uint8_t bri   = noise;
+
+    leds[i] = ColorFromPalette(currentPalette, index, bri, LINEARBLEND);   // With that value, look up the 8 bit colour palette value and assign it to the current LED.
+  }
+
+} // noise16_1()
+
+
 
